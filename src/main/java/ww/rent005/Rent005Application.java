72d7631a212be1bc074350a8ff02147b75f860b2
cronos <e867854215@qq.com>
2020-01-29 16:50:30 +0800

    Project Build.

---
+++ src/main/java/ww/rent005/Rent005Application.java
@@ -0,0 +1,13 @@
+package ww.rent005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rent005Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rent005Application.class, args);
+    }
+
+}
---
+++ src/test/java/ww/rent005/Rent005ApplicationTests.java
@@ -0,0 +1,13 @@
+package ww.rent005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rent005ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
